5ffd530a30fb9e7530247c1204a0cecc085f42f4
binary_ho <dfghcvb11@naver.com>
2023-06-20 13:30:39 +0900

    hello test!

---
+++ src/main/java/com/binaryho/testjjangjjangman/TestJjangjjangmanApplication.java
@@ -0,0 +1,13 @@
+package com.binaryho.testjjangjjangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJjangjjangmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJjangjjangmanApplication.class, args);
+	}
+
+}
