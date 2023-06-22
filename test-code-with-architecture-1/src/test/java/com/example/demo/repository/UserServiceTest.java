d59676fcec2aa0cbd1b537c382a580aee7a5b662
binary_ho <dfghcvb11@naver.com>
2023-06-22 23:30:36 +0900

    test : MockBean을 이용해 doNothing Stubbing 테스트 코드 작성

--- test-code-with-architecture-1/src/test/java/com/example/demo/repository/UserServiceTest.java
+++ test-code-with-architecture-1/src/test/java/com/example/demo/repository/UserServiceTest.java
@@ -2,12 +2,20 @@ package com.example.demo.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
 import com.example.demo.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -50,7 +58,7 @@ public class UserServiceTest {
     void getById은_ACTIVE_상태인_유저를_찾아올_수_있다() {
         // Given
         // When
-        UserEntity result = userService.getById(1L);
+        UserEntity result = userService.getById(2L);
 
         // Then
         assertThat(result.getNickname()).isEqualTo("binary-ho");
@@ -61,7 +69,33 @@ public class UserServiceTest {
         // Given
         // When
         // Then
-        assertThatThrownBy(() -> userService.getById(2L))
+        assertThatThrownBy(() -> userService.getById(3L))
             .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @MockBean
+    private JavaMailSender mailSender;
+
+    @Test
+    void UserCreateDto_를_이용하여_유저를_생성할_수_있다() {
+        // Given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+            .email("test@naver.com")
+            .address("Mapo")
+            .nickname("jinhoho")
+            .build();
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        // When
+        UserEntity result = userService.create(userCreateDto);
+
+        // Then
+        assertAll(
+            () -> assertThat(result.getId()).isNotNull(),
+            () -> assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING),
+            () -> assertThat(result.getEmail()).isEqualTo("test@naver.com"),
+            () -> assertThat(result.getAddress()).isEqualTo("Mapo"),
+            () -> assertThat(result.getNickname()).isEqualTo("jinhoho")
+        );
     }
 }
