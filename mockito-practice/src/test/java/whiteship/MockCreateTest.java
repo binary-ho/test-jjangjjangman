499478ce1ecea6b9072ec940fb34b0f6a8a1da00
binary_ho <dfghcvb11@naver.com>
2023-06-22 10:52:34 +0900

    test : h2를 이용한 repository test

--- mockito-practice/src/test/java/whiteship/MockCreateTest.java
+++ mockito-practice/src/test/java/whiteship/MockCreateTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.context.ApplicationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
 public class MockCreateTest {
---
+++ test-code-with-architecture-1/src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'dfghcvb11@naver.com', 'binary-ho', 'Mapo', 'aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
